19e98aea95dc098ef8faa42ad8e34164bff08f35
raiaman <amnrai8@gmail.com>
2025-11-19 17:02:57 +0530

    Billchangesdne

--- ScurryDashboard/wwwroot/js/site.js
+++ ScurryDashboard/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿@media print {
+    @page {
+        size: 58mm;
+        margin: 0;
+    }
+    body {
+        margin: 0!important;
+        padding: 0!important;
+        width: 58mm!important;
+    }
+    #bill - modal,
+        #bill - modal * {
+            visibility: visible!important;
+        }
+    #bill - modal.modal - content {
+        width: 58mm!important;
+        box - shadow: none!important;
+        border: none!important;
+        margin: 0!important;
+        padding: 0!important;
+    }
+}
 
-// Write your JavaScript code.
